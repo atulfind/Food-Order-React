104d6ec90765bc77a03063f853cf34a3487a5cfd
atul <atulfind@gmail.com>
2019-11-17 20:54:10 +0530

    Updates

---
+++ precache-manifest.6c4595745abb43e542cc743fc022eeb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cbfe28f081d3f14d9bd0dcd372443d5",
+    "url": "/lunch-order/index.html"
+  },
+  {
+    "revision": "638f8c1ae8250ca860dd",
+    "url": "/lunch-order/static/css/main.3174cff3.chunk.css"
+  },
+  {
+    "revision": "8afe748072ee05afafe7",
+    "url": "/lunch-order/static/js/2.68349604.chunk.js"
+  },
+  {
+    "revision": "638f8c1ae8250ca860dd",
+    "url": "/lunch-order/static/js/main.48ff11e7.chunk.js"
+  },
+  {
+    "revision": "7ffa348ab345fb8acfc5",
+    "url": "/lunch-order/static/js/runtime-main.79d95b6f.js"
+  }
+]);
